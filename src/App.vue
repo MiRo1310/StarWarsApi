aa4973f9b9361a06ec129c0bb0653a2a414cf3f4
MiRo1310 <michael.roling@gmx.de>
2022-11-06 19:22:58 +0100

    First Comit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-black text-yellow-400 text-center">
+    <h1 class="  text-7xl p-7 ">{{ title.toLocaleUpperCase() }} </h1>
+    <p class="text-xl p-4">{{ planets.length }} Planet aus dem Star Wars Universum</p>
+  </div>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div v-for="(planet, index) in planets" :key="planet">
+
+      <StarWarsPlanet :planetprob="planet" :indexprob="index" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { appendFile } from 'fs';
+import StarWarsPlanet from './components/StarWarsPlanet.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    StarWarsPlanet,
+  },
+  data() {
+    return {
+      title: "Star Wars",
+      planets: [],
+      apiURL: "https://swapi.dev/api/planets/"
+    }
+  },
+  mounted() {
+    this.getData(this.apiURL)
+  },
+  methods: {
+    async getData(url) {
+      try {
+        const response = await this.axios.get(url)
+        this.planets = response.data.results
+        console.log(response.data.results)
+      }catch (err){
+        console.log(err)
+      }
+
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/StarWarsPlanet.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="bg-black text-center py-12 border-2 border-yellow-200 m-5 ">
+    <h2 class="text-yellow-400 text-2xl">{{ planet.name }}</h2>
+    <button class="bg-blue-200 rounded p-2 text-gray-700 mt-5" v-on:click="toggleShow">Show Info</button>
+    <div v-if="show">
+      <p class="text-yellow-400 my-6 underline">Info des Planeten</p>
+      <ul class="text-white">
+
+        <li v-for="(value, key) in planet" :key="value">          
+          {{checkKey(key, value)}}
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StarWarsPlanet",
+  components: {
+    
+  },
+  data() {
+    return {
+      planet: this.planetprob,
+      show: false
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.show = !this.show;
+    },
+    firstLetterToUpperCase(name) {
+      const firstLetter = name.slice(0, 1); // Ersten Buchstaben selektieren
+      const leftoverLetters = name.slice(1); // Restliche Buchstaben selektieren
+      name = firstLetter.toUpperCase() + leftoverLetters; // Erster Buchstabe in Groß + Rest
+
+      return name;
+    },
+    checkKey(key, value){      
+      if (!(key == "name" || key == "films" || key == "residents")){   
+        return `${this.firstLetterToUpperCase(key)}: ${value}`
+      }else if (key == "residents" ){
+        return `${this.firstLetterToUpperCase(key)}: ${this.planet[key].length}`
+      }else if (key == "films" ){
+        return `${this.firstLetterToUpperCase(key)}: ${this.planet[key].length}`
+      }
+    }
+  },
+  props: ["planetprob", "indexprob"],
+
+}
+</script>
