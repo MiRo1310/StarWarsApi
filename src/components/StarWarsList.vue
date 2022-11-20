bd2d36eb56e5f8f5125710173e504fb2725fbc80
MiRo1310 <michael.roling@gmx.de>
2022-11-20 15:51:24 +0100

    function firstLetterToUpperCase fixed

--- src/components/StarWarsList.vue
+++ src/components/StarWarsList.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="bg-black text-center py-8 border-2 border-yellow-200 m-5 ">
-    <h2 class="text-yellow-400 text-2xl underline cursor-pointer" v-on:click="loadSide(valueAPI.url)">{{ valueAPI.name }}{{ valueAPI.title }}</h2>
-    
+    <h2 class="text-yellow-400 text-2xl underline cursor-pointer" v-on:click="loadSide(valueAPI.url)">{{ valueAPI.name
+    }}{{ valueAPI.title }}</h2>
+
     <button class="bg-blue-200 rounded p-2 text-gray-700 my-5" v-on:click="toggleShow">Show / Hide Info</button>
-    <div v-if="show">      
+    <div v-if="show">
       <ul class="text-white">
 
-        <li v-for="(value, key) in valueAPI" v-bind:key="value">         
+        <li v-for="(value, key) in valueAPI" v-bind:key="value">
           <template v-if="Array.isArray(value)">
             <p>{{
-              firstLetterToUpperCase(key)
-              }}: </p>
-            <div v-for="val in value" v-bind:key="val[1]">              
-              <a v-on:click="loadSide(val)" class="underline text-yellow-400 my-6" href="#">{{ val}}</a>
+                firstLetterToUpperCase(key)
+            }}: </p>
+            <div v-for="val in value" v-bind:key="val[1]">
+              <a v-on:click="loadSide(val)" class="underline text-yellow-400 my-6" href="#">{{ val }}</a>
             </div>
-          </template>         
+          </template>
 
-          <p v-else class="px-6">{{ firstLetterToUpperCase(key) }}: {{firstLetterToUpperCase(value)}}</p>
+          <p v-else class="px-6">{{ firstLetterToUpperCase(key) }}: {{ firstLetterToUpperCase(value) }}</p>
 
         </li>
 
@@ -34,27 +35,30 @@ export default {
   data() {
     return {
       valueAPI: this.valueAPIprob,
-      show: this.oneInfoProb,      
+      show: this.oneInfoProb,
     };
   },
-  computed:{
-    
+  computed: {
+
   },
   methods: {
     toggleShow() {
       this.show = !this.show;
     },
-    loadSide(url){      
+    loadSide(url) {
       this.$emit("loadSide-url", url)
     },
 
-    firstLetterToUpperCase(name) {      
-      
-      const firstLetter = name.slice(0, 1); // Ersten Buchstaben selektieren
-      const leftoverLetters = name.slice(1); // Restliche Buchstaben selektieren
-      name = firstLetter.toUpperCase() + leftoverLetters; // Erster Buchstabe in Groß + Rest
+    firstLetterToUpperCase(name) {
 
-      return name;    
+      if (typeof (name) == "string") {
+        const firstLetter = name.slice(0, 1); // Ersten Buchstaben selektieren
+        const leftoverLetters = name.slice(1); // Restliche Buchstaben selektieren
+        name = firstLetter.toUpperCase() + leftoverLetters; // Erster Buchstabe in Groß + Rest
+
+
+      }
+      return name;
     },
 
   },
